a98e2e5e31b7bc1f7e44c4a91aaee2a345d0a725
ChrigorG <99369312+ChrigorG@users.noreply.github.com>
2023-08-31 20:09:41 -0400

    Delete database/database.js

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize("guiaperguntas", "root", "L@zag1228", {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
